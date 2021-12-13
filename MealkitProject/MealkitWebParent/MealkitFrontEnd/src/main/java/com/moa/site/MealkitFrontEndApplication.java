145f586e9f6b609520b2d29656a7513433fed8ca
josephsssss <josephsssss@hotmail.com>
2021-12-13 19:17:27 +0900

    refacotring mealkit project

---
+++ MealkitProject/MealkitWebParent/MealkitFrontEnd/src/main/java/com/moa/site/MealkitFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.moa.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealkitFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealkitFrontEndApplication.class, args);
+	}
+
+}
